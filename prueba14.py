df57fde207439b8a78266451efdbbfd0cd0e19e8
g0ldust <164261133+g0ldust@users.noreply.github.com>
2026-06-25 20:56:56 -0400

    prueba 14

--- prueba14.py
+++ prueba14.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-# 1. IMPORTA ESTAS TRES LÍNEAS NUEVAS
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 from integraciones.integracionMantis import enviar_resultado_mantis, obtener_proyectos_mantis
@@ -17,18 +14,28 @@ try:
     driver.get(url)
     driver.maximize_window()
         
-    # 2. REMPLAZA LA BÚSQUEDA DIRECTA POR UNA ESPERA EXPLÍCITA
-    # Esto esperará hasta 10 segundos a que el elemento sea visible y cliqueable
-    boton_categoria = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//*[@id='tienda']/div/div[2]/a[3]"))
-    )
+    
+    inputs = driver.find_elements(By.TAG_NAME, "input")
+    
 
+    time.sleep(3)
+        
+   
+    boton_categoria = driver.find_element(By.XPATH, "//*[@id='tienda']/div/div[2]/a[2]")
+
+  
+  
     boton_categoria.click()
+        
+      
         
     time.sleep(3) 
 
     driver.save_screenshot("evidencia.png")
         
+    
+
+
     time.sleep(3)
     
     obtener_proyectos_mantis()
@@ -40,5 +47,6 @@ try:
         "evidencia.png"
     )
 
+  
 finally :
     driver.quit()
